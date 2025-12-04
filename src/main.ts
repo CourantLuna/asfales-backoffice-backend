8657c2da71fe826b5a76e61207945d02e0ed289d
courantluna <courantluna@gmail.com>
2025-12-04 16:02:30 -0400

    Initial commit - Asfales backoffice Backend

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',   // frontend
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.listen(process.env.NEXT_PUBLIC_BACKEND_URL ?? 3001);
+}
+bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { FirebaseAuthGuard } from '../guards/firebase-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  @UseGuards(FirebaseAuthGuard)
+  @Get('me')
+  getMe(@Req() req) {
+    const firebaseUser = req.user;
+
+    return {
+      id: firebaseUser.uid,
+      firebaseUid: firebaseUser.uid,
+      email: firebaseUser.email,
+      name: firebaseUser.name ?? '',
+      avatar: firebaseUser.picture ?? '',
+      createdAt: firebaseUser.auth_time,
+      phoneNumber: firebaseUser.phoneNumber
+    };
+  }
+}
